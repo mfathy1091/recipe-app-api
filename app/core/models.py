93331be6aeab6bf9eb1f556ea65da6542176eb44
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-05-03 21:31:20 +0300

    normalizing new users emails

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
