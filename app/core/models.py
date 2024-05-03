99b8664d2315dfff916d87f7ba4e8658f4732996
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-05-03 20:47:24 +0300

    using custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,21 @@
-from django.db import models  # noqa
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
