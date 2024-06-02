104251699072e66c6005fcc9bdaf244364eb202d
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-02 21:01:42 +0300

    adding string representation to Category model

--- app/expenses/models.py
+++ app/expenses/models.py
@@ -11,6 +11,10 @@ class Category(models.Model):
     name = models.CharField(unique=True, blank=False, null=False, max_length=50)
     type = models.CharField(choices=CATEGORY_TYPE.choices, max_length=50)
 
+    def __str__(self):
+        return self.name
+
 class Transaction(models.Model):
     amount = models.DecimalField(max_digits=10,decimal_places=2, default=0)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
