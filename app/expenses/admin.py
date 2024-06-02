f0d1af447ccbefef503c21b1ac88b36ea5b84e60
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-02 20:56:13 +0300

    Registering Category model in admin panel

--- app/expenses/admin.py
+++ app/expenses/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Transaction
+from .models import Category, Transaction
 
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("amount", )
+    list_display = ("amount", "category")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "type")
