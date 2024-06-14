35b3021002e7436ccb7da877091d2f174a3f4df7
Mohamed Fathy <mohammad.soliman44@gmail.com>
2024-06-14 09:47:06 +0300

    Creating Account and Category serializers

--- app/expenses/serializers.py
+++ app/expenses/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from .models import Transaction
+from .models import Account, Transaction
 
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['name']
 
 class TransactionSerializer(serializers.ModelSerializer):
+    account = AccountSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
     class Meta:
         model = Transaction
         fields = ['amount', 'category', 'type', 'date', 'notes', 'account']
