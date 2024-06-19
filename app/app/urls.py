4fa84db64523215ce94f4767f94de22bfc7ecf14
Mohameed Fathy <mohammad.soliman44@gmail.com>
2024-06-19 10:29:26 +0300

    Added urls for Category and Account

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import include, path
-from expenses.urls import transaction_router
+from expenses.urls import Category_router, account_router, transaction_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/transactions/', include(transaction_router.urls))
+    path('api/transactions/', include(transaction_router.urls)),
+    path('api/categories/', include(Category_router.urls)),
+    path('api/accounts/', include(account_router.urls))
 ]
--- app/expenses/urls.py
+++ app/expenses/urls.py
@@ -1,6 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
-from .viewsets import TransactionViewSet
+from .viewsets import AccountViewSet, CategoryViewSet, TransactionViewSet
 
 transaction_router = DefaultRouter()
 transaction_router.register(r'', TransactionViewSet, basename='transactions')
+
+account_router = DefaultRouter()
+account_router.register(r'', AccountViewSet, basename='accounts')
+
+Category_router = DefaultRouter()
+Category_router.register(r'', CategoryViewSet, basename='categories')
