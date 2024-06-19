86fe7766a223d9676df6474a034b8b3c94bce093
Mohameed Fathy <mohammad.soliman44@gmail.com>
2024-06-19 10:29:13 +0300

    Added Viewsets for Category and Account

--- app/expenses/viewsets.py
+++ app/expenses/viewsets.py
@@ -1,9 +1,20 @@
 from rest_framework import viewsets
 
-from .models import Transaction
-from .serializers import TransactionSerializer
+from .models import Account, Category, Transaction
+from .serializers import (AccountSerializer, CategorySerializer,
+                          TransactionSerializer)
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
