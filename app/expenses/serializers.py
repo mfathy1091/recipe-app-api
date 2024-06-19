428b143b4e7ccaa29c86b45d01eb880124360217
Mohameed Fathy <mohammad.soliman44@gmail.com>
2024-06-19 10:28:35 +0300

    Added slug field to Category and Account serializers

--- app/expenses/serializers.py
+++ app/expenses/serializers.py
@@ -3,19 +3,29 @@ from rest_framework import serializers
 from .models import Account, Transaction
 
 
-class AccountSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Account
-        fields = ['name']
+class BaseSerializer(serializers.ModelSerializer):
+    slug = serializers.SerializerMethodField()
 
-class CategorySerializer(serializers.ModelSerializer):
+    def get_slug(self, obj):
+        return obj.name.lower().replace(' ', '-')
+
+
+class AccountSerializer(BaseSerializer):
     class Meta:
         model = Account
-        fields = ['name']
+        fields = ['name', 'slug']
+
+
+class CategorySerializer(BaseSerializer):
+    class Meta:
+        model = Account
+        fields = ['name', 'slug']
+
 
 class TransactionSerializer(serializers.ModelSerializer):
     account = AccountSerializer(read_only=True)
     category = CategorySerializer(read_only=True)
+
     class Meta:
         model = Transaction
         fields = ['amount', 'category', 'type', 'date', 'notes', 'account']
